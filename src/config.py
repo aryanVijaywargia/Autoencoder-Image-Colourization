753d672ef929a4f9a4525dea3d8466d4fe33580e
aryanVijaywargia <aryanvijaywargia@gmail.com>
2021-04-08 03:19:43 +0530

    added config.py python file

---
+++ src/config.py
@@ -0,0 +1,7 @@
+BEST_MODEL_PATH = "models/model.hdf5"
+SIZE=160
+EPOCHS=50
+BATCH_SIZE=50
+BASE_PATH_COLOR='../input/landscape-image-colorization/landscape Images/color'
+BASE_PATH_BW='../input/landscape-image-colorization/landscape Images/gray'
+NUM_TEST_FILES=500
